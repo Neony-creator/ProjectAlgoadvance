6ee4c95ebbb9280a3fd559db6fafa89d3d826c58
[hugoktz] <[hugokretz67@gmail.com]>
2023-06-13 16:22:53 +0200

    Ajout du graph

--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import warnings
 import networkx as nx
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 outputFile = "./LogTest.txt"
@@ -171,7 +172,7 @@ def aoc(matrix, iterations, ants, cities, evaporation, intensification, writer, 
 
 
 if __name__ == '__main__':
-    size = 10
+    size = int(input("Veuillez entrer la taille de la matrice: ")) #Demande la taille de la matrice
     # m = np.random.uniform(1, 10, size=(size, size))
 
     # np.fill_diagonal(m, 0)
@@ -187,7 +188,7 @@ if __name__ == '__main__':
     print("Runtime : ", time.time() - timer)
     nbTest = modifier_premiere_ligne(outputFile)
     with open(outputFile, 'a') as writer:
-        best = aoc(m, iterations=10, ants=10, cities=size, evaporation=0.1, intensification=2, writer=writer, nbTest= nbTest)
+        best = aoc(m, iterations=100, ants=10, cities=size, evaporation=0.1, intensification=2, writer=writer, nbTest= nbTest)
 
     print("Best : ", best)
     print("Process : ", time.process_time())
@@ -200,8 +201,7 @@ edge_labels = nx.get_edge_attributes(G, 'weight')
 
 #Dessin du graphe avec les poids des liens
 pos = nx.spring_layout(G)  # Positionnement des nœuds
-nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, edge_color='gray')
+nx.draw(G, pos, with_labels=True, node_color='green', node_size=500, edge_color='gray')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)  # Affichage des poids des liens
 
-#Affichage du graphe
-plt.show(
+#plt.show() #Affichage du graphe en commentaire car fait crash le pc en size=1000
